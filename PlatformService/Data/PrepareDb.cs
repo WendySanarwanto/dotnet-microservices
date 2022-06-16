6a231b044b36cf1e04f6eaa4f1e8e849046ad30a
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-06-17 04:14:52 +0800

    PlatformService: Added Configuration to connect to MSSQL Server.

--- PlatformService/Data/PrepareDb.cs
+++ PlatformService/Data/PrepareDb.cs
@@ -1,26 +1,37 @@
+using Microsoft.EntityFrameworkCore;
 using PlatformService.Models;
 
 namespace PlatformService.Data {
     public static class PrepareDb {
         public static void PrepPopulation(WebApplication app) {
             using(var serviceScope = app.Services.CreateScope()) {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), 
+                        app.Environment.IsProduction());
             }
         }
 
-        private static void SeedData(AppDbContext dbContext) {
-            if (!dbContext.Platforms.Any()) {
-                Console.WriteLine("--> Seeding data ...");
+        private static void SeedData(AppDbContext dbContext, bool isProduction = false) {
+            if (!isProduction) {
+                if (!dbContext.Platforms.Any()) {
+                    Console.WriteLine("--> Seeding data on In-Memory Database ...");
 
-                dbContext.Platforms.AddRange(
-                    new Platform { Name="Dot Net", Publisher="Microsoft", Cost="Free"},
-                    new Platform { Name="SQL Server Express", Publisher="Microsoft", Cost="Free"},
-                    new Platform { Name="Kubernetes", Publisher="Cloud Native Computing Foundation", Cost="Free"}
-                );
+                    dbContext.Platforms.AddRange(
+                        new Platform { Name="Dot Net", Publisher="Microsoft", Cost="Free"},
+                        new Platform { Name="SQL Server Express", Publisher="Microsoft", Cost="Free"},
+                        new Platform { Name="Kubernetes", Publisher="Cloud Native Computing Foundation", Cost="Free"}
+                    );
 
-                dbContext.SaveChanges();
+                    dbContext.SaveChanges();
+                } else {
+                    Console.WriteLine("--> We already have data.");
+                }
             } else {
-                Console.WriteLine("--> We already have data.");
+                Console.WriteLine("---> Attempting to apply migrations to SQL Server Database ...");                
+                try {
+                    dbContext.Database.Migrate();
+                } catch(Exception exception) {
+                    Console.WriteLine($"---> Cannot run migrations: {exception.Message}. ");
+                }
             }
         }
     }
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -5,26 +5,30 @@ using PlatformService.SyncDataServices.Http;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<AppDbContext>(options => 
-    options.UseInMemoryDatabase("InMemDb")
-);
-// builder.Services.AddDbContext<AppDbContext>(
-//     options =>options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")) );
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
+// NOTE: Comment these lines before running : dotnet-ef migrations command
+// ---------------------
 if (builder.Environment.IsDevelopment()){
     Console.WriteLine("--> Generating swagger doc ...");
     builder.Services.AddSwaggerGen();
+    Console.WriteLine("--> Using In-Memory database ...");
+    builder.Services.AddDbContext<AppDbContext>(options => 
+        options.UseInMemoryDatabase("InMemDb"));
+} else if (builder.Environment.IsProduction()) {
+// ---------------------
+    Console.WriteLine("--> Using SQL Server database ...");
+    builder.Services.AddDbContext<AppDbContext>(
+        options =>options.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsSqlConnection")) 
+    );
 }
-// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -41,6 +45,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// NOTE: Comment this line before running : dotnet-ef migrations command
 PrepareDb.PrepPopulation(app);
 
 Console.WriteLine($"---> CommandService Endpont {builder.Configuration["CommandService"]}");

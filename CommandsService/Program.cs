5f7a43451555799aaf545468a9623c190d87ccdd
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2022-06-16 15:48:56 +0800

    CommandService: Initial Project Scaffolding

---
+++ CommandsService/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+if (builder.Environment.IsDevelopment()){
+    Console.WriteLine("--> Preparing swagger doc ...");
+    builder.Services.AddSwaggerGen();
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    Console.WriteLine("--> Adding swagger doc ...");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
